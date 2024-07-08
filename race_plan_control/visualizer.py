d83d4b812311275646a3c60617d837ee24dc42bf
Majid Khonji <majid.khonji@gmail.com>
2024-07-08 05:46:45 +0400

    Added simulation: a basic kinematics engine
    UI is also updated to include control signals

--- race_plan_control/visualizer.py
+++ race_plan_control/visualizer.py
@@ -40,6 +40,9 @@ class PlotApp(tk.Tk):
         ## UI Elements for Visualize - Buttons
         global_frame = ttk.Frame(self.visualize_frame)
         global_frame.pack(fill=tk.X, padx=5, pady=5)
+        self.vehicle_state_label = ttk.Label(global_frame, text="")
+        self.vehicle_state_label.pack(side=tk.RIGHT)
+
         ttk.Label(global_frame, text="Global Coordinate:").pack(anchor=tk.W, side=tk.LEFT)
         ttk.Button(global_frame, text="Zoom In", command=self.zoom_in).pack(side=tk.LEFT)
         ttk.Button(global_frame, text="Zoom Out", command=self.zoom_out).pack(side=tk.LEFT)
@@ -51,38 +54,49 @@ class PlotApp(tk.Tk):
         coordinate_frame = ttk.Frame(self.visualize_frame)
         coordinate_frame.pack(fill=tk.X, padx=5, pady=5)
 
-        # Control frame
-        self.control_frame = ttk.LabelFrame(self, text="Control (Dummy Planner)")
-        self.control_frame.pack(fill=tk.X, side=tk.BOTTOM, padx=10, pady=5)
+        # Plan frame
+        self.plan_frame = ttk.LabelFrame(self, text="Plan")
+        self.plan_frame.pack(fill=tk.X, side=tk.LEFT, padx=10, pady=5)
 
         ## UI Elements for Control
-        wp_frame = ttk.Frame(self.control_frame)
+        wp_frame = ttk.Frame(self.plan_frame)
         wp_frame.pack(fill=tk.X, padx=5, pady=5)
         ttk.Button(wp_frame, text="Set Waypoint", command=self.set_wp).pack(side=tk.LEFT)
         self.timestep_entry = ttk.Entry(wp_frame)
+        self.timestep_entry.insert(0, "0")
         self.timestep_entry.pack(side=tk.LEFT)
 
-        ttk.Button(self.control_frame, text="Animate", command=self.start_animation).pack(side=tk.LEFT)
-        ttk.Button(self.control_frame, text="Stop", command=self.stop_animation).pack(side=tk.LEFT)
-        ttk.Button(self.control_frame, text="Replan", command=self.replan).pack(side=tk.LEFT, padx=30)
-        ttk.Button(self.control_frame, text="Step", command=self.step_to_next_wp).pack(side=tk.LEFT)
-        ttk.Button(self.control_frame, text="Prev", command=self.step_to_prev_wp).pack(side=tk.LEFT)
+        ttk.Button(self.plan_frame, text="Animate", command=self.start_animation).pack(side=tk.LEFT)
+        ttk.Button(self.plan_frame, text="Stop", command=self.stop_animation).pack(side=tk.LEFT)
+        ttk.Button(self.plan_frame, text="Replan", command=self.replan).pack(side=tk.LEFT, padx=30)
+        ttk.Button(self.plan_frame, text="Step", command=self.step_to_next_wp).pack(side=tk.LEFT)
+        ttk.Button(self.plan_frame, text="Prev", command=self.step_to_prev_wp).pack(side=tk.LEFT)
 
+        # Control Frame
+        self.control_frame = ttk.LabelFrame(self, text="Control (simulate)")
+        self.control_frame.pack(fill=tk.X, side=tk.LEFT, padx=10, pady=5)
+        dt_frame = ttk.Frame(self.control_frame)
+        dt_frame.pack(fill=tk.X, padx=5, pady=5)
+        ttk.Label(dt_frame, text="Δt ").pack(side=tk.LEFT)
+        self.dt_entry = ttk.Entry(dt_frame)
+        self.dt_entry.insert(0, "0.1")
+        self.dt_entry.pack(side=tk.LEFT)
 
-        # Plotting setup
-        # self.fig = Figure(figsize=(5, 4), dpi=100)
-        # gs = GridSpec(2, 1, height_ratios=[3, 1], figure=self.fig)  # Adjust the height ratios as needed
-        # self.ax1 = self.fig.add_subplot(gs[0])
-        # self.ax2 = self.fig.add_subplot(gs[1])  
-
+        ttk.Button(self.control_frame, text="Control Step", command=self.step_to_next_wp).pack(side=tk.LEFT)
+        
+        ttk.Button(self.control_frame, text="Steer Left", command=self.step_steer_left).pack(side=tk.LEFT)
+        ttk.Button(self.control_frame, text="Steer Right", command=self.step_steer_right).pack(side=tk.LEFT)
+        ttk.Button(self.control_frame, text="Accelerate", command=self.step_acc).pack(side=tk.LEFT)
+        ttk.Button(self.control_frame, text="Deccelerate", command=self.step_dec).pack(side=tk.LEFT)
 
         self.xy_zoom = 30
         self.frenet_zoom = 15
         self.fig = util.fig
         self.ax1 = util.ax1
         self.ax2 = util.ax2
-        # self.pl.plot()
-        util.plot(self.pl)
+        
+        self.car = util.Car(speed=30)
+        util.plot(self.pl, car=self.car)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)  # A tk.DrawingArea.
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
@@ -99,29 +113,30 @@ class PlotApp(tk.Tk):
             # Optionally, clear the coordinates display when the mouse is not over the axes
             self.coordinates_label.config(text="")
 
+
     def update_plot(self):
         # Placeholder for the method to update the plot based on the checkboxes
         pass
 
 
     def zoom_in(self):
-        self.xy_zoom -= 10 if self.pl.xy_zoom > 10 else 0
-        util.plot(self.pl, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.xy_zoom -= 5 if self.xy_zoom > 5 else 0
+        util.plot(self.pl,self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
         self.canvas.draw()
 
     def zoom_out(self):
-        self.xy_zoom += 10
-        util.plot(self.pl, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.xy_zoom += 5
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
         self.canvas.draw()
 
     def zoom_in_frenet(self):
-        self.frenet_zoom -= 10 if self.frenet_zoom > 10 else 0 
-        util.plot(self.pl, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.frenet_zoom -= 5 if self.frenet_zoom > 5 else 0 
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
         self.canvas.draw()
 
     def zoom_out_frenet(self):
-        self.frenet_zoom += 10
-        util.plot(self.pl, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.frenet_zoom += 5 
+        util.plot(self.pl,self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
         self.canvas.draw()
     
 
@@ -130,10 +145,11 @@ class PlotApp(tk.Tk):
         print("value is ", self.timestep_entry.get())
         timestep_value = int(self.timestep_entry.get())
         self.pl.step_idx = timestep_value
+        # TODO 
 
     def replan(self):
         self.pl.replan()
-        util.plot(self.pl, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
         self.canvas.draw()
     
     def start_animation(self):
@@ -146,11 +162,47 @@ class PlotApp(tk.Tk):
     def step_to_next_wp(self):
         # Placeholder for the method to step to the next waypoint
         self.pl.step()
-        util.plot(self.pl, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.car.set_state(self.pl.xdata[-1], self.pl.ydata[-1])
+        self.vehicle_state_label.config(text=f"Vehicle State: X: {self.car.x:.2f}, Y: {self.car.y:.2f}, Speed: {self.car.speed:.2f}, Theta: {self.car.theta:.2f}")
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
         self.canvas.draw()
+        self.timestep_entry.delete(0, tk.END)
+        self.timestep_entry.insert(0, str(self.pl.race_trajectory.next_wp-1)) 
     def step_to_prev_wp(self):
         # Placeholder for the method to step to the previous waypoint
         pass
+
+    def step_control(self):
+        pass
+
+    def step_steer_left(self):
+        dt = float(self.dt_entry.get())
+        self.car.step(dt = dt, steering_angle = 0.1)
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.canvas.draw()
+        self.vehicle_state_label.config(text=f"Vehicle State: X: {self.car.x:.2f}, Y: {self.car.y:.2f}, Speed: {self.car.speed:.2f}, Theta: {self.car.theta:.2f}")
+
+    def step_steer_right(self):
+        dt = float(self.dt_entry.get())
+        self.car.step(dt = dt, steering_angle = -0.1)
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.canvas.draw()
+        self.vehicle_state_label.config(text=f"Vehicle State: X: {self.car.x:.2f}, Y: {self.car.y:.2f}, Speed: {self.car.speed:.2f}, Theta: {self.car.theta:.2f}")
+        
+    def step_acc(self):
+        dt = float(self.dt_entry.get())
+        self.car.step(dt = dt, acceleration = 0.1)
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.canvas.draw()
+        self.vehicle_state_label.config(text=f"Vehicle State: X: {self.car.x:.2f}, Y: {self.car.y:.2f}, Speed: {self.car.speed:.2f}, Theta: {self.car.theta:.2f}")
+
+    def step_dec(self):
+        dt = float(self.dt_entry.get())
+        self.car.step(dt = dt, acceleration = -0.1)
+        util.plot(self.pl, self.car, xy_zoom=self.xy_zoom, frenet_zoom=self.frenet_zoom)
+        self.canvas.draw()
+        self.vehicle_state_label.config(text=f"Vehicle State: X: {self.car.x:.2f}, Y: {self.car.y:.2f}, Speed: {self.car.speed:.2f}, Theta: {self.car.theta:.2f}")
+    
 
 
 def load():
