c51382439dd2ba7d2873b8aa99408e3dbaef5067
Majid Khonji <majid.khonji@gmail.com>
2024-12-04 13:41:14 +0400

    minor updates

--- race_plan_control/main.py
+++ race_plan_control/main.py
@@ -33,9 +33,7 @@ def load_config(config_path):
 
     pkg_config = pkg_resources.resource_filename("race_plan_control", "config.yaml")
     if is_running_from_source:
-        current_file_name = (
-            os.path.realpath(__file__) if sys.argv[0] == "" else sys.argv[0]
-        )
+        current_file_name = os.path.realpath(__file__) if sys.argv[0] == "" else sys.argv[0]
     else:
         current_file_name = pkg_config if sys.argv[0] == "" else sys.argv[0]
 
@@ -53,15 +51,16 @@ def load_config(config_path):
     # loading race trajectory data
     with open(path_to_track, "r") as f:
         track_data = json.load(f)
-        reference_path = [
-            point[:2] for point in track_data["ReferenceLine"]
-        ]  # ignoring z
+        reference_path = [point[:2] for point in track_data["ReferenceLine"]]  # ignoring z
         ref_left_boundary_d = track_data["LeftBound"]
         ref_right_boundary_d = track_data["RightBound"]
     logging.info(f"Track data loaded from {path_to_track}")
 
     return reference_path, ref_left_boundary_d, ref_right_boundary_d
 
+
+def reload_imports():
+    pass
 
 def get_executer():
     global reference_path, ref_left_boundary_d, ref_right_boundary_d
@@ -85,14 +84,13 @@ def get_executer():
     importlib.reload(race_plan_control.execute.executer)
     importlib.reload(race_plan_control.perceive.state)
 
+
     RNDPlanner = race_plan_control.plan.sampling_planner.RNDPlanner
     PIDController = race_plan_control.control.pid.PIDController
     SimpleSim = race_plan_control.execute.executer.Executer
     VehicleState = race_plan_control.perceive.state.VehicleState
 
-    state = VehicleState(
-        x=reference_path[0][0], y=reference_path[0][1], speed=30, theta=-np.pi / 4
-    )
+    state = VehicleState(x=reference_path[0][0], y=reference_path[0][1], speed=30, theta=-np.pi / 4)
     pl = RNDPlanner(
         reference_path=reference_path,
         ref_left_boundary_d=ref_left_boundary_d,
@@ -106,9 +104,7 @@ def get_executer():
 
 def run(config_path="config.yaml", headless=False):
     global reference_path, ref_left_boundary_d, ref_right_boundary_d
-    reference_path, ref_left_boundary_d, ref_right_boundary_d = load_config(
-        config_path=config_path
-    )
+    reference_path, ref_left_boundary_d, ref_right_boundary_d = load_config(config_path=config_path)
     executer = get_executer()
     app = VisualizerApp(executer, code_reload_function=get_executer)
     app.mainloop()
