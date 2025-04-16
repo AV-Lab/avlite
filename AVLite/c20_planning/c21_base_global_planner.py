ef2715abbe74e5878e47717c4f453109e79888ec
Majid Khonji <majid.khonji@gmail.com>
2025-04-17 02:58:41 +0400

    added start and goal for global plan

--- AVLite/c20_planning/c21_base_global_planner.py
+++ AVLite/c20_planning/c21_base_global_planner.py
@@ -4,9 +4,6 @@ from enum import Enum
 import networkx as nx
 
 
-class PlannerType(Enum):
-    RACE_PLANNER = "Race Planner"
-    HD_MAP_PLANNER = "HD Map Plannr"
 
 
 @dataclass
@@ -19,6 +16,10 @@ class GlobalPlan:
     right_boundary_d: list[float] = field(default_factory=list)
     left_boundary_path: list[tuple[float, float]] = field(default_factory=list)
     right_boundary_path: list[tuple[float, float]] = field(default_factory=list)
+    start_point: tuple[float, float] = (0.0, 0.0)
+    goal_point: tuple[float, float] = (0.0, 0.0)
+
+
 
 class BaseGlobalPlanner(ABC):
     registry = {}
@@ -28,9 +29,16 @@ class BaseGlobalPlanner(ABC):
         self.graph = nx.DiGraph()
 
     @abstractmethod
-    def plan(self, start: tuple[float, float], goal: tuple[float, float]) -> None:
+    def plan(self) -> None:
+        """Plan a path from start to goal."""
         pass
 
+    
+    def set_start_goal(self, start_point: tuple[float, float], goal_point: tuple[float, float]) -> None:
+        """Set start and goal points for the planner."""
+        self.global_plan.start_point = start_point
+        self.global_plan.goal_point = goal_point
+        
     def __init_subclass__(cls, abstract=False, **kwargs):
         super().__init_subclass__(**kwargs)
         if not abstract:  # only register non-abstract subclasses
