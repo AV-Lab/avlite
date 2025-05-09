32fc502aa9263927983dbb7af291dc2b2dc14edc
Majid Khonji <majid.khonji@gmail.com>
2025-05-09 05:07:50 +0400

    global road planning working: 90%. Some bugs still persist, namely filtering out roads without driving lanes

--- AVLite/c20_planning/c22_global_planning_strategy.py
+++ AVLite/c20_planning/c22_global_planning_strategy.py
@@ -1,16 +1,16 @@
-from dataclasses import dataclass, field
+import logging
 from abc import ABC, abstractmethod
 import networkx as nx
 
 from c20_planning.c21_planning_model import GlobalPlan
 
+log = logging.getLogger(__name__)
 
 class GlobalPlannerStrategy(ABC):
     registry = {}
 
     def __init__(self):
         self.global_plan: GlobalPlan = GlobalPlan()
-        self.graph = nx.DiGraph()
 
     @abstractmethod
     def plan(self) -> None:
@@ -20,8 +20,8 @@ class GlobalPlannerStrategy(ABC):
     
     def set_start_goal(self, start_point: tuple[float, float], goal_point: tuple[float, float]) -> None:
         """Set start and goal points for the planner."""
-        self.global_plan.start = start_point
-        self.global_plan.goal = goal_point
+        self.global_plan.start_point = start_point
+        self.global_plan.goal_point = goal_point
         
     def __init_subclass__(cls, abstract=False, **kwargs):
         super().__init_subclass__(**kwargs)
